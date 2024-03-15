e412c7631d3fcc66d68f16b717ac81a866895058
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-03-15 21:27:41 +0100

    Paginate repo function

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import requests
 import json
 
-# Global variables for the number of stars and page number
-NUMBER_OF_STARS = 200
-PAGE_NUMBER = 1
-
-def fetch_github_names(stars, page):
-    url = f"https://github.com/search?q=stars%3A{stars}&type=repositories&p={page}"
-    response = requests.get(url)
+def fetch_github_repos(stars, num_of_repos):
     
-    if response.status_code == 200:
-        # Parse the JSON content
-        json_data = json.loads(response.text)
+    current_page = 1
+    results = []
+    
+    while len(results) <= num_of_repos:
+    
+        url = f"https://github.com/search?q=stars%3A{stars}&type=repositories&p={current_page}"
+        response = requests.get(url)
         
-        # Navigate to the 'payload' and then 'results' part of the JSON
-        if 'payload' in json_data and 'results' in json_data['payload']:
-            # Extract 'hl_name' strings from each item in 'results'
-            hl_names = [item['hl_name'] for item in json_data['payload']['results']]
-        else:
-            hl_names = []
-        
-        return hl_names
-    else:
-        return "Failed to fetch page"
+        if response.status_code == 200:
+            # Parse the JSON content
+            json_data = json.loads(response.text)
+            
+            # Navigate to the 'payload' and then 'results' part of the JSON
+            if 'payload' in json_data and 'results' in json_data['payload']:
+                # Extract 'hl_name' strings from each item in 'results'
+                hl_names = [item['hl_name'] for item in json_data['payload']['results']]
+            else:
+                hl_names = []
+                
+            results.extend(hl_names)
+            
+        current_page += 1
+            
+    return results[:num_of_repos]
+
+# Global variables for the number of stars and page number
+star_filter = 200
+num_of_repos_to_fetch = 25
 
 # Example usage
-hl_names = fetch_github_names(NUMBER_OF_STARS, PAGE_NUMBER)
-print(hl_names)
+repos = fetch_github_repos(star_filter, num_of_repos_to_fetch)
+print(repos)
+print(len(repos))
